37f21e92220dc509757f086a410ecb3bcdcdd60f
vikto <vikto@DESKTOP-CBSCN3M>
2021-11-23 16:54:19 +0500

    Добавьте файлы проекта.

---
+++ pract11_zad2/pract11_zad2.cpp
@@ -0,0 +1,134 @@
+﻿
+
+#include <thread>
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+int const n = 5;
+int mas[n][n];
+int count_double_digit = 0;
+
+void loop()
+{
+    srand(time(0));
+    int a = rand() % 999 - 0;
+    for (int i = 0; i < n; i++)
+    {
+        a = rand() % 999 - 0;
+        mas[0][i] = a;
+        cout << mas[0][i] << " ";
+        if ((mas[0][i] > 9) && (mas[0][i] < 100)) 
+        {
+            count_double_digit++;
+        }
+        
+    }
+    cout << endl;
+}
+void loop1()
+{
+    srand(time(0));
+    int a = rand() % 999 - 0;
+    for (int i = 0; i < n; i++)
+    {
+        a = rand() % 999 - 0;
+        mas[1][i] = a;
+        cout << mas[1][i] << " ";
+        if ((mas[1][i] > 9) && (mas[1][i] < 100))
+        {
+            count_double_digit++;
+        }
+    }
+    cout << endl;
+}
+void loop2()
+{
+    srand(time(0));
+    int a = rand() % 999 - 0;
+    for (int i = 0; i < n; i++)
+    {
+        a = rand() % 999 - 0;
+        mas[2][i] = a;
+        cout << mas[2][i] << " ";
+        if ((mas[2][i] > 9) && (mas[2][i] < 100))
+        {
+            count_double_digit++;
+        }
+    }
+    cout << endl;
+}
+void loop3()
+{
+    srand(time(0));
+    int a = rand() % 999 - 0;
+    for (int i = 0; i < n; i++)
+    {
+        a = rand() % 999 - 0;
+        mas[3][i] = a;
+        cout << mas[3][i] << " ";
+        if ((mas[3][i] > 9) && (mas[3][i] < 100))
+        {
+            count_double_digit++;
+        }
+    }
+    cout << endl;
+}
+void loop4()
+{
+    srand(time(0));
+    int a = rand() % 999 - 0;
+    for (int i = 0; i < n; i++)
+    {
+        a = rand() % 999 - 0;
+        mas[4][i] = a;
+        cout << mas[4][i] << " ";
+        if ((mas[4][i] > 9) && (mas[4][i] < 100))
+        {
+            count_double_digit++;
+        }
+    }
+    cout << endl;
+}
+int main()
+{
+    int max = 0;
+    int min = 1000;
+
+    thread th(loop);
+    th.join();
+    Sleep(1000);
+    thread th1(loop1);
+    th1.join();
+    Sleep(1000);
+    thread th2(loop2);
+    th2.join();
+    Sleep(1000);
+    thread th3(loop3);
+    th3.join();
+    Sleep(1000);
+    thread th4(loop4);
+    th4.join();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (mas[i][j] > max)
+            {
+                max = mas[i][j];
+            }
+
+            if (mas[i][j] < min)
+            {
+                min = mas[i][j];
+            }
+        }
+    }
+    setlocale(LC_ALL, "Ukrainian");
+    cout << "Количество 2-значных чисел: " << count_double_digit <<endl;
+    cout << "Максимальное: " << max << endl << "Минимальное: " << min << endl;
+    return 0;
+}
---
+++ pract11_zad3/pract11_zad3.cpp
@@ -0,0 +1,59 @@
+﻿
+#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <iostream>
+#include<windows.h>
+#include "atlstr.h"
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+int _tmain(int argc, char* argv[])
+    {
+        if (argc == 1)
+        {
+            STARTUPINFO si = { sizeof(STARTUPINFO) };
+            PROCESS_INFORMATION pi;
+            ZeroMemory(&si, sizeof(STARTUPINFO));
+            TCHAR szCmdLine[] = TEXT("myprog.exe");
+            TCHAR str[64];
+            BOOL bRet;
+            HANDLE THandle;
+            HANDLE hEvent = CreateEvent(NULL, true, true, NULL);  //создаю событие
+            DuplicateHandle(GetCurrentProcess(), hEvent, GetCurrentProcess(), &THandle, 0, FALSE, DUPLICATE_SAME_ACCESS); //дублирую его дескриптор
+
+            HANDLE* hD = new HANDLE[3]; //массив процессов
+            for (int i = 0; i < 3; i++)
+            {
+                bRet = CreateProcess(NULL, szCmdLine, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi); //пораждаю экземпляр
+                hD[i] = pi.hProcess;
+                CloseHandle(pi.hThread);
+            }
+            CloseHandle(hEvent);
+            OpenEvent(EVENT_MODIFY_STATE, true, NULL);
+
+            WaitForSingleObject(THandle, INFINITY);
+            for (int i = 0; i < 3; i++)
+                CloseHandle(hD[i]);
+        }
+        else if (argc == 3)
+        {
+            cout << argv[1] << endl;
+            int c = 0;
+            DWORD dwResult;
+            HANDLE hEvent = argv[2];
+            while (c < 3)
+            {
+                dwResult = WaitForSingleObject(hEvent, INFINITE);
+                if (WAIT_FAILED != dwResult)
+                {
+                    for (int j = 0; j < 10; j++) {
+                        cout << j << endl;
+                        Sleep(200);
+                    }
+                    c++;
+                    SetEvent(hEvent);
+                }
+            }
+        }
+        return 0;
+}
